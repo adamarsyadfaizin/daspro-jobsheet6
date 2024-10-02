08780109ace0643f8581c4ac6773ed2f6ba067c3
adamarsyadfaizin <adamkian09@gmail.com>
2024-10-02 04:19:27 +0000

    MyCommitPertanyaan

--- Pemilihan2Percobaan1_14.java
+++ Pemilihan2Percobaan1_14.java
@@ -8,12 +8,15 @@ public class Pemilihan2Percobaan1_14 {
         int tahun = input14.nextInt();
         
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
+            if ((tahun % 100) == 0 ) { 
+                if ((tahun % 400) == 0) {
                 System.out.println("Tahun Kabisat");
             } else {
                 System.out.println("Bukan Tahun Kabisat");
             }
         } else {
+            System.out.println("Bukan Tahun Kabisat");
+        } } else {
             System.out.println("Bukan Tahun Kabisat");
         }
     }
