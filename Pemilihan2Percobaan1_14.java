a1fe20948dc3cc269502253440c47e71afc51fb4
adamarsyadfaizin <adamkian09@gmail.com>
2024-10-02 04:08:31 +0000

    MyCommitModification

--- Pemilihan2Percobaan1_14.java
+++ Pemilihan2Percobaan1_14.java
@@ -6,10 +6,15 @@ public class Pemilihan2Percobaan1_14 {
         Scanner input14 = new Scanner(System.in);
         System.out.print("Masukkan tahun : ");
         int tahun = input14.nextInt();
+        
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0)
-            System.out.println("Tahun Kabisat");
-        } else
-        System.out.println("Bukan Tahun Kabisat");
+            if ((tahun % 100) != 0 || (tahun % 400) == 0) {
+                System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
